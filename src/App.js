e8364a50197d5fa7513cec707c2c82a09ea75b72
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2020-09-26 20:05:49 +0530

    2nd Assignment

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Menu from "./components/MenuComponent";
-import { DISHES } from "./shared/dishes";
+import Main from "./components/MainComponent";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES,
-    };
-  }
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
+    <BrowserRouter>
+    <div className="App">
+      <Main/>
+    </div>
+    </BrowserRouter>
     );
   }
 }
